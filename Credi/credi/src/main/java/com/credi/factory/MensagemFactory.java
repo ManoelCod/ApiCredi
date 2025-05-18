2427931e42df626219c64285d5a7c698bc596141
Manoel Neto <manoelcontatoneto@gmail.com>
2025-05-18 17:47:19 -0300

    feat: Design patterns - aplicando refatoracao para o projeto

--- Credi/credi/src/main/java/com/credi/factory/MensagemFactory.java
+++ Credi/credi/src/main/java/com/credi/factory/MensagemFactory.java
@@ -4,5 +4,4 @@ public class MensagemFactory {
     public static String criarLogAuditoria(String servico, String metodo, String mensagem) {
         return String.format("Serviço: %s | Método: %s | Mensagem: %s", servico, metodo, mensagem);
     }
-    // Adicione outros tipos de mensagem conforme necessário
 }
